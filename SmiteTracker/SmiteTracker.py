1d52651f8adabdffe82993f6c590842704822e69
Oogisoft <eddie@oogisoft.com>
2025-11-23 01:40:44 -0600

    init

--- SmiteTracker/SmiteTracker.py
+++ SmiteTracker/SmiteTracker.py
@@ -8,7 +8,7 @@ webhook_url = 'https://discord.com/api/webhooks/1204479951911264327/GvJ2P5oPcQ9u
 eric_id = '472887827139526656'
 
 
-
+phrases = ['phrase 1: ', 'phrase 2: ']
 with sync_playwright() as pw:
 
     browser = pw.chromium.launch(
@@ -48,15 +48,29 @@ with sync_playwright() as pw:
     # Extract rank alt text
     rank = page.locator("img.image.object-contain").first.get_attribute("alt")
     last_rank_game = page.locator('div.text-18.font-bold.text-secondary').first.text_content()
+    # Extract win loss text
+
+    page.goto('https://tracker.gg/smite2/profile/psn/Pearadoxx/overview?gamemode=conquest&season=3', wait_until="domcontentloaded")
+    wins = page.locator("span.value.text-green.truncate").first.text_content()
+    losses = page.locator("span.value.text-red.truncate").first.text_content()
+
+
+    #phrase creation for message to discord
     cur_rank = f"<@{eric_id}> Eric is currently Rank: " + str(rank)
     last_ranked = "the last time Eric played Ranked was " + str(last_rank_game)
+    todays_WL = f'His record for today is {wins + losses}'
+    
+    
+    
     browser.close()
 
-webhook = DiscordWebhook(url=webhook_url, content= cur_rank + ' and ' + last_ranked)
+webhook = DiscordWebhook(url=webhook_url, content= cur_rank + ' and ' 
+                         + last_ranked + ' and ' + todays_WL)
 
 response = webhook.execute()
-
 if response.status_code == 204:
     print('succesfully ran')
 else:
     print(f'I failed somewhere, check this, {response.status_code}, Response: {response.text}')
+
+print(wins, losses)
