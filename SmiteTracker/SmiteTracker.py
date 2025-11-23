3a6543eec6bf168669debd320e8979ce752e928d
Oogisoft <eddie@oogisoft.com>
2025-11-23 02:06:13 -0600

    init

--- SmiteTracker/SmiteTracker.py
+++ SmiteTracker/SmiteTracker.py
@@ -3,20 +3,19 @@
 from playwright.sync_api import sync_playwright
 from discord_webhook import DiscordWebhook
 
-smite_profile = 'https://tracker.gg/smite2/profile/psn/Pearadoxx'
+smite_rank = 'https://tracker.gg/smite2/profile/psn/Pearadoxx/overview?gamemode=conquest-ranked&season=3'
+smite_casual = 'https://tracker.gg/smite2/profile/psn/Pearadoxx/overview?gamemode=conquest&season=3'
 webhook_url = 'https://discord.com/api/webhooks/1204479951911264327/GvJ2P5oPcQ9ubZuzm3w9L9O5VeKRKMIrd0iqkSAcISEVMM0W94VezJb7bdKyjbXBgD_O'
 eric_id = '472887827139526656'
 
-
+#fun phrases to randomize message
+#TO DO
 phrases = ['phrase 1: ', 'phrase 2: ']
+##
 with sync_playwright() as pw:
-
-    browser = pw.chromium.launch(
-        headless=False,
-        args=[
-            "--disable-blink-features=AutomationControlled"
-        ]
-    )
+    #setup Edge as browser for PW
+    browser = pw.chromium.launch(headless=False,
+        args=["--disable-blink-features=AutomationControlled"])
 
     context = browser.new_context(
         user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
@@ -36,11 +35,11 @@ with sync_playwright() as pw:
 
     page = context.new_page()
 
-    # slight delay helps with bot checks
+    # slight delay for bot checks
     page.wait_for_timeout(500)
 
-    # Go to the page
-    page.goto(smite_profile, wait_until="domcontentloaded")
+    # Go to the rank page first
+    page.goto(smite_rank, wait_until="domcontentloaded")
 
     # Wait for the specific rank image to appear
     page.wait_for_selector("img.image.object-contain")
@@ -48,31 +47,38 @@ with sync_playwright() as pw:
     # Extract rank alt text
     rank = page.locator("img.image.object-contain").first.get_attribute("alt")
     last_rank_game = page.locator('div.text-18.font-bold.text-secondary').first.text_content()
-    # Extract win loss text
+    rank_wins = page.locator("span.value.text-green.truncate").first.text_content()
+    rank_losses = page.locator("span.value.text-red.truncate").first.text_content()
+    
+    
+    #Extract win loss text
     #need to load new page for this data
-    page.goto('https://tracker.gg/smite2/profile/psn/Pearadoxx/overview?gamemode=conquest&season=3', wait_until="domcontentloaded")
-    wins = page.locator("span.value.text-green.truncate").first.text_content()
-    losses = page.locator("span.value.text-red.truncate").first.text_content()
+    page.goto(smite_casual, wait_until="domcontentloaded")
+    cas_wins = page.locator("span.value.text-green.truncate").first.text_content()
+    cas_losses = page.locator("span.value.text-red.truncate").first.text_content()
 
 
     #phrase creation for message to discord
-    cur_rank = f"<@{eric_id}> Eric is currently Rank: " + str(rank)
-    last_ranked = "the last time Eric played Ranked was " + str(last_rank_game)
-    todays_WL = f'His record for today is {wins} {losses}'
+    cur_rank = str(rank)
+    last_ranked = str(last_rank_game)
+    todays_cas_WL = f'{cas_wins} {cas_losses}'
+    todays_rank_WL = f'{rank_wins} {rank_losses}'
     
     
     
     browser.close()
-
-
+phrase1 = f'<@a_friend> is rank {cur_rank} but hasn\'t played ranked since {last_rank_game}'
+phrase2 = f'Eric\'s ranked W/L is {todays_rank_WL}'
+phrase3 = f'Eric\'s casual W/L is {todays_cas_WL}'
+print(f'<@a_friend> is rank {cur_rank} but hasn\'t played ranked since {last_rank_game}')
+print(f'Eric\'s ranked W/L is {todays_rank_WL}')
+print(f'Eric\'s casual W/L is {todays_cas_WL}')
 #Discord execution is below, mute for testing
-webhook = DiscordWebhook(url=webhook_url, content= cur_rank + ' and ' 
-                         + last_ranked + ' and ' + todays_WL)
+webhook1 = DiscordWebhook(url=webhook_url, content=f"""{phrase1} \n {phrase2} \n {phrase3}""")
 
-response = webhook.execute()
+response = webhook1.execute()
 if response.status_code == 204:
     print('succesfully ran')
 else:
     print(f'I failed somewhere, check this, {response.status_code}, Response: {response.text}')
 
-print(wins, losses)
